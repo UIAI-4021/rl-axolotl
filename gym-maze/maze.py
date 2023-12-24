276333cc906fcda90d9c730dc4420e27bc46fb50
MehrAzin Marzough <mehrazin.marzough@gmail.com>
2023-12-24 12:09:11 +0330

    matplot and step limit

--- gym-maze/maze.py
+++ gym-maze/maze.py
@@ -1,10 +1,13 @@
+import matplotlib.pyplot as plt
 import numpy as np
-import random
 import gym
+import matplotlib.pyplot as plot
 import gym_maze
 
-# Create the maze environment
+# Create an environment
 env = gym.make("maze-random-10x10-plus-v0")
+observation = env.reset()
+
 
 # Discretize the continuous state space
 def discretize_state(state):
@@ -20,49 +23,57 @@ def discretize_state(state):
 
     return tuple(discretized_state)
 
+
 # Initialize Q-table with zeros
 num_actions = env.action_space.n
 Q = np.zeros((10, 10, num_actions))  # Assuming a 2D state space with 10x10 discretized values
 
 # Q-learning parameters
 alpha = 0.5  # Learning rate
-gamma = 0.9  # Discount factor
+gamma = 0.99  # Discount factor
 epsilon = 0.99  # Epsilon-greedy parameter
 min_epsilon = 0.1  # Minimum epsilon value
 epsilon_decay = 0.99  # Epsilon decay rate
 
 # Maximum episodes
-NUM_EPISODES = 1000
+NUM_EPISODES = 2000
+episodes = list()
 
 for episode in range(NUM_EPISODES):
-    state = discretize_state(env.reset())
+    State = discretize_state(env.reset())
     total_reward = 0
+    steps = 0
+    done = False
 
-    while True:
-        env.render()
-
-        # Epsilon-greedy policy for action selection
-        if random.uniform(0, 1) < epsilon:
-            action = env.action_space.sample()  # Random action
-        else:
-            action = np.argmax(Q[state])  # Greedy action based on Q-values
+    while not done and steps <= 100:
+        # env.render()
+        steps += 1
+        action = np.argmax(Q[State])  # Greedy action based on Q-values
 
         # Perform the chosen action
         next_state, reward, done, _ = env.step(action)
         next_state = discretize_state(next_state)
 
         # Update Q-value for the state-action pair
-        Q[state][action] += alpha * (reward + gamma * np.max(Q[next_state]) - Q[state][action])
+        Q[State][action] += alpha * (reward + gamma * np.max(Q[next_state]) - Q[State][action])
 
         total_reward += reward
-        state = next_state
+        State = next_state
 
         if done:
-            print(f"Episode: {episode + 1}, Total Reward: {total_reward}")
+            episodes.append(episode)
+            print(f"Episode: {episode + 1}, Total Reward: {total_reward}, epsilon: {epsilon}")
             break
 
     # Epsilon decay after each episode
     epsilon = max(min_epsilon, epsilon * epsilon_decay)
 
+y = list()
+for i in range(len(episodes) - 1):
+    y.append(episodes[i+1] - episodes[i])
+y.append(0)
+x = episodes
+plot.plot(x,y)
+plot.show()
 # Close the environment
 env.close()
