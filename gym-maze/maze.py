3f35f882e2b468648dc9c56ed5aeaeaf74f857fc
Aliakbar Ahrari <ahr04624@gmail.com>
2023-12-26 01:16:19 +0330

    Document added

--- gym-maze/maze.py
+++ gym-maze/maze.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import gym
 import matplotlib.pyplot as plot
@@ -10,7 +9,7 @@ observation = env.reset()
 
 # Initialize Q-table with zeros
 num_actions = env.action_space.n
-Q = np.zeros((100, num_actions))  # Assuming a 2D state space with 10x10 discretized values
+Q = np.zeros((100, num_actions))
 
 # Q-learning parameters
 alpha = 0.1  # Learning rate
@@ -19,9 +18,9 @@ epsilon = 0.99  # Epsilon-greedy parameter
 min_epsilon = 0.1  # Minimum epsilon value
 epsilon_decay = 0.99  # Epsilon decay rate
 
-# Maximum episodes
-NUM_EPISODES = 1000
-episodes = list()
+NUM_EPISODES = 1000  # Maximum episodes
+total_finds = 0  # Total number of times the agent finishes the maze
+episodes = list()  # Episodes agent finished the maze
 
 for episode in range(NUM_EPISODES):
 
@@ -33,7 +32,7 @@ for episode in range(NUM_EPISODES):
     done = False
 
     while not done and steps <= 100:
-        # env.render()
+        env.render()
 
         action = np.argmax(Q[state])  # Greedy action based on Q-values
 
@@ -48,8 +47,8 @@ for episode in range(NUM_EPISODES):
         state = next_state
 
         if done:
+            total_finds += 1
             episodes.append(episode)
-            print(f"Wins at Episode: {episode + 1}, Total Reward: {total_reward}, epsilon: {epsilon}")
             break
 
         steps += 1
@@ -57,6 +56,7 @@ for episode in range(NUM_EPISODES):
     # Epsilon decay after each episode
     epsilon = max(min_epsilon, epsilon * epsilon_decay)
 
+print(f'Winning rate:   {(total_finds / NUM_EPISODES)}')  # print the winning-rate
 y = list()
 for i in range(len(episodes) - 1):
     y.append(episodes[i + 1] - episodes[i])
